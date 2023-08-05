14b74d24ba5977ae6cb7296a1b531d78fbb22540
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-08-05 19:08:49 +0700

    Fix some things

--- ffmp-armv6/Application.mk
+++ ffmp-armv6/Application.mk
@@ -1,10 +1,10 @@
 #  Copyright © 2023 Dmitry Tretyakov (aka. Tinelix)
 #
-#  This file as part of FFmpeg custom builder.
+#  This file as part of FFmpeg custom builder for Android.
 #
-#  FFmpeg custom builder is free software: you can redistribute it and/or modify it under the terms of
-#  the GNU Lesser General Public License as published by the Free Software Foundation, either
-#  version 3 of the License, or (at your option) any later version.
+#  FFmpeg custom builder for Android is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by the Free Software
+#  Foundation, either version 3 of the License, or (at your option) any later version.
 #  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 #  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 #  See the GNU Lesser General Public License for more details.
