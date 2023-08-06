616d260f6406a2316e7e83f109f0ebe455a8283b
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-08-06 12:42:55 +0700

    Update Android.mk

--- ndk-project-example/Android.mk
+++ ndk-project-example/Android.mk
@@ -7,15 +7,23 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifeq($(TARGET_ARCH_ABI),armeabi)
+	ANDROID_ARCH = armv6
+ifeq($(TARGET_ARCH_ABI),armeabi-v7a)
+	ANDROID_ARCH = armv7
+else
+	ANDROID_ARCH = armv8
+endif
+
 FFMPEG_VERSION := 4.0.4
 
 LOCAL_MODULE := ffmpeg-prebuilt
 
-LOCAL_SRC_FILES := ../ffmpeg/android/$(TARGET_ARCH)/libffmpeg-v${FFMPEG_VERSION}.so
+LOCAL_SRC_FILES := ../ffmpeg/android/${ANDROID_ARCH}/libffmpeg-v${FFMPEG_VERSION}.so
 
-LOCAL_EXPORT_C_INCLUDES := ../ffmpeg/android/$(TARGET_ARCH)/include
+LOCAL_EXPORT_C_INCLUDES := ../ffmpeg/android/${ANDROID_ARCH}/include
 
-LOCAL_EXPORT_LDLIBS := ../ffmpeg/android/$(TARGET_ARCH)}/libffmpeg-v${FFMPEG_VERSION}.so
+LOCAL_EXPORT_LDLIBS := ../ffmpeg/android/${ANDROID_ARCH}/libffmpeg-v${FFMPEG_VERSION}.so
 
 LOCAL_PRELINK_MODULE := true
 
@@ -25,9 +33,9 @@ include $(CLEAR_VARS)
 LOCAL_ALLOW_UNDEFINED_SYMBOLS=false
 LOCAL_MODULE := ffmpeg-player
 LOCAL_SRC_FILES := ffmpeg-player.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/android/$(TARGET_ARCH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/android/${ANDROID_ARCH}/include
 LOCAL_SHARED_LIBRARY := ffmpeg-prebuilt
-LOCAL_LDLIBS    := -llog -ljnigraphics -lz -lm $(LOCAL_PATH)/../ffmpeg/android/$(TARGET_ARCH)/libffmpeg-v${FFMPEG_VERSION}.so
+LOCAL_LDLIBS    := -llog -ljnigraphics -lz -lm $(LOCAL_PATH)/../ffmpeg/android/${ANDROID_ARCH}/libffmpeg-v${FFMPEG_VERSION}.so
 
 include $(BUILD_SHARED_LIBRARY)
 
